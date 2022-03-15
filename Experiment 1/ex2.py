39e34e8e020a4bcf470ae1932cd716aee8f5ba8b
Shawn Huang <shawn.huang.y.h@gmail.com>
2022-03-15 21:46:06 +0800

    fix: add error handling
    Signed-off-by: Shawn Huang <shawn.huang.y.h@gmail.com>

--- Experiment 1/ex2.py
+++ Experiment 1/ex2.py
@@ -1,7 +1,10 @@
-from datetime import datetime, timedelta
+from datetime import *
 
 if __name__ == '__main__':
-    time_raw = input('Please input the time(HH:MM:SS):')
-    time = datetime.strptime(time_raw, '%H:%M:%S')
-    time += timedelta(seconds=1)
-    print(time.strftime('%H:%M:%S'))
+    try:
+        time_raw = input('Please input the time(HH:MM:SS):')
+        time = datetime.strptime(time_raw, '%H:%M:%S')
+        time += timedelta(seconds=1)
+        print(time.strftime('%H:%M:%S'))
+    except:
+        print("Invalid input")
