78a1ed3d9559afb0e59e590012874c1141909252
Shawn Huang <shawn.huang.y.h@gmail.com>
2022-04-28 13:49:47 +0800

    feat: add figure for ex3

--- Experiment 2/ex3.py
+++ Experiment 2/ex3.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 
 def sum_same_birthday(student):
@@ -22,8 +23,20 @@ def get_calculation(m, n):
 
 
 if __name__ == '__main__':
-    m = int(input("Input class numbers(M):"))
-    n = int(input("Input student numbers in each class(N):"))
-    p, q = get_calculation(m, n)
-    print("Q={}".format(q))
-    print("P={}".format(p))
+    choice = input("Use manual input?(y/N)")
+    if choice == 'y' or choice == 'Y':
+        m = int(input("Input class numbers(M):"))
+        n = int(input("Input student numbers in each class(N):"))
+        p, q = get_calculation(m, n)
+        print("Q={}".format(q))
+        print("P={}".format(p))
+    else:
+        x = list()
+        y = list()
+        m = 100
+        for i in range(300):
+            p, q = get_calculation(m, i)
+            x.append(i)
+            y.append(p)
+        plt.plot(x, y)
+        plt.show()
